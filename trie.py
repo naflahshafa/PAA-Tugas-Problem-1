a7762189ad468e75b076f21334b9d6efcb0a085e
Naflah Shafa E <103978992+naflahshafa@users.noreply.github.com>
2023-05-31 13:04:34 +0800

    Update trie.py

--- trie.py
+++ trie.py
@@ -74,6 +74,17 @@ class Trie:
         for char, child in node.children.items():
             self._collect_child_nodes(child, prefix + char, child_nodes)
 
+    def search_by_prefix(self, prefix):
+        current = self.root
+        for char in prefix:
+            if char not in current.children:
+                return []
+            current = current.children[char]
+
+        words = []
+        self._collect_child_nodes(current, prefix, words)
+        return words
+
     def print_structure(self):
         self._print_structure_helper(self.root, "")
 
@@ -83,6 +94,21 @@ class Trie:
 
         for char, child in node.children.items():
             self._print_structure_helper(child, prefix + char)
+
+    def search_by_letters(self, letters):
+        matches = []
+        node = self._traverse(letters)
+        if node:
+            self._collect_child_nodes(node, letters, matches)
+        return matches
+
+    def _traverse(self, letters):
+        current = self.root
+        for char in letters:
+            if char not in current.children:
+                return None
+            current = current.children[char]
+        return current
 
 
 # Contoh penggunaan kamus Trie
@@ -95,25 +121,37 @@ kamus.insert("ginger")
 kamus.insert("apple")
 kamus.insert("papaya")
 kamus.insert("banana")
+kamus.insert("butter")
 kamus.insert("ant")
 kamus.insert("grape")
 kamus.insert("water")
 kamus.insert("camel")
+kamus.insert("butterfly")
 kamus.insert("grapefruit")
 kamus.insert("bear")
 kamus.insert("corn")
 kamus.insert("watermelon")
 
 # Pencarian kata dalam kamus
-print(kamus.search("apple"))  # True
-print(kamus.search("mango"))  # True
-print(kamus.search("laptop"))  # False
+print(kamus.search("apple"))
+print(kamus.search("mango"))
+print(kamus.search("laptop"))
+print("-----")
+
+# Pencarian kata berdasarkan huruf awal
+words_with_prefix = kamus.search_by_prefix("g")
+print(words_with_prefix)
+print("-----")
+
+# Pencarian kata dengan huruf yang diberikan
+matches = kamus.search_by_letters("bu")
+print(matches)
 print("-----")
 
 # Pencarian node parent
 parent_node = kamus.find_parent_node("watermelon")
 if parent_node:
-    print("Parent node:", parent_node.children.keys())  # ['f']
+    print("Parent node:", parent_node.children.keys())
 else:
     print("Parent node not found")
 print("-----")
@@ -121,14 +159,14 @@ print("-----")
 # Pencarian node child
 child_nodes = kamus.find_child_nodes("water")
 if child_nodes:
-    print("Child nodes:", child_nodes)  # ['grapefruit']
+    print("Child nodes:", child_nodes)
 else:
     print("Child nodes not found")
 print("-----")
 
 # Penghapusan kata dari kamus
 kamus.delete("apple")
-print(kamus.search("apple"))  # False
+print(kamus.search("apple"))
 print("-----")
 
 # Tampilkan struktur pohon Trie
